e3812603917004e985816a4fd4cd54b7a07672b5
Musharraf8543 <musharrafhussain4397@gmail.com>
2022-09-27 17:52:58 +0530

    tip calculator pushed to git

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+window.onload=()=>{
+    document.querySelector('#calculate').onclick=calculateTip;
+}
+function calculateTip(){
+    var bill=document.querySelector('#billamt').value;
+    var tippercent=document.querySelector('#serviceQual').value;
+    var people=document.querySelector('#prsnamt').value;
+    if(bill == '' || tippercent === 'Select' || people === '') {
+        alert("Please Enter Bill Amount");
+        return;
+    }
+    if(people === '1'){
+        document.querySelector('#each').style.display = 'none';
+    }else{
+        document.querySelector('#each').style.display = 'block';
+    }
 
+    var tipcalculate=(bill*tippercent)/people;
+    tipcalculate=tipcalculate.toFixed(2);
+    document.getElementById("total").innerHTML=tipcalculate;
+}
